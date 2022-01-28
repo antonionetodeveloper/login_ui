86cd232764c420ee3fdc96e06571ceca726fc5fe
Antônio <antonio.fernandes.de.santana.neto@gmail.com>
2022-01-27 21:13:54 -0300

    update login page

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import svg from "./svg.svg"
@@ -6,9 +5,6 @@ import { Container } from "./styles";
 import { Button } from "../../Components/button";
 
 export const Login = () => {
-	const [login, setLogin] = useState()
-	const [pass, setPass] = useState()
-  // "login" e "pass" são os valores de login, para uma possível altenticação.
 
   return (
     <Container>
@@ -16,8 +12,8 @@ export const Login = () => {
         <div>
           <h1>Entrar</h1>
           <form onSubmit={() => {alert(`o site só tem o propósito de mostrar a parte visual, e não de ser funcional. Suas informações não serão guardadas em nenhum lugar, Não se preocupe.`)}}>
-            <input placeholder="Email" type="email"    onChange={(props) => setLogin(props.target.value)}/>
-            <input placeholder="Senha" type="password" onChange={(props) => setPass(props.target.value)} />
+            <input placeholder="Email" type="email"   />
+            <input placeholder="Senha" type="password"/>
             <Button text="Entrar"/>
           </form>
           <p>Ainda não tem? <Link to={"/cadastro"}>Crie uma conta</Link></p>
